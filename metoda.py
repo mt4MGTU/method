31688a714c8d092eafdc6dc77ce338e7eed17a58
МТ4_МГТУ <33216678+mt4MGTU@users.noreply.github.com>
2017-11-01 12:17:23 +0300

    Update metoda.py

--- metoda.py
+++ metoda.py
@@ -37,44 +37,7 @@ def IntervalOfErr(meanValue, dispersion, p, dispersionOfDinamic=0):  # IntervalO
     intervalOfErr2 = round(meanValue - coefK * sigmaOfMeasuring, 3)
     return intervalOfErr1, intervalOfErr2
 
-# # к методу 2
-#
-# def SDOfBasicErr(error): # SDOfBasicErr - СКО основной погрешности
-# # Error - предел допускаемого значения основной погрешности i-го блока ИИС
-#     return math.fabs(error)/1.7
-#
-# def ValueOfAdditionalErr(diffOfInfluenceQuantity, coef, influenceQuantity, valueOfInfluenceQuantity, errOfInfluenceQuantity): #ValueOfAdditionalErr - наибольшее возможное значение дополнительной погрешности
-# # DiffOfInfluenceQuantity - приращение j-й влияющей величины, для которой нормирована МХ; Coef - коэф-т;
-# #InfluenceQuantity - влияющая вел-на;ValueOfInfluenceQuantity - значение влияющей вел-ны;
-# # ErrOfInfluenceQuantity - допускаемое изменение  погрешности i-го блока ИИС, вызванное отклонением j-й влияющей величины от норм. знач-я
-#     if influenceQuantity == valueOfInfluenceQuantity:
-#         coef = 0
-#     elif influenceQuantity != valueOfInfluenceQuantity:
-#         coef = 1
-#     elif errOfInfluenceQuantity == diffOfInfluenceQuantity: #!!!!!!!!!!!!!!!!!!!!!!!!!!1КАЖИСЬ ВСЕ РАВНО НЕ ПОНЯТНО
-#         coef = math.fabs(influenceQuantity-valueOfInfluenceQuantity)/diffOfInfluenceQuantity
-#     return errOfInfluenceQuantity*coef
-#
-# def SDOfAdditionalErrInfluenceQuantity(valueOfAdditionalErr): # SDOfAdditionalErrInfluenceQuantity - СКО дополнительной погрешности i-го блока ИИС от j-й влияющей величины
-# #ValueOfAdditionalErr - наибольшее возможное значение дополнительной погрешности
-#     return math.fabs(valueOfAdditionalErr)/1.7
-#
-# def SDOfRandomErr(sdOfBasicErr, numbOfInfluenceQuantity, sdOfAdditionalErrInfluenceQuantity): #SDOfRandomErr - СКО случайной погрешности блока;
-# #  NumbOfInfluenceQuantity - количество влияющих величин
-# #sdOfBasicErr - СКО дополнительной погрешности i-го блока ИИС от j-й влияющей величины
-#     for i in range(numbOfInfluenceQuantity):
-#         sdOfBasicErr += 2 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!НА ЧТО УВЕЛИЧИВАЕТСЯ?!
-#     return math.sqrt(sdOfAdditionalErrInfluenceQuantity**2+ sdOfBasicErr) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!ОПЯТЬ НЕПОНЯТНО КАК СУММУ СЧИТАТЬ
-#
-# def SDOfRandomErrOfIK(numbOfBlocks, sdOfRandomErr): #SDOfRandomErrOfIK - СКО случайной погрешности ИК
-# #  NumbOfBlocks - количество блоков, входящих в состав ИК
-#     for i in range(numbOfBlocks):
-#         sdOfRandomErr += 2 #НЕПОНЯТНО ОПЯТЬ,ТОЖЕ СУММА
-#     return math.sqrt(sdOfRandomErr**2)
-#
-# def IntervalOfErr2(sdOfRandomErrOfIK, confidProbability): #IntervalOfError2 - границы интервала погрешности ИК;
-# # ConfidProbability - вероятность (P)
-#     return sdOfRandomErrOfIK*confidProbability
+# # к методу 2 в стадии доработки !
 
 # тело проограммы
 
@@ -111,43 +74,3 @@ if metod == 1:
     p = 0.9
     intervalOfErr1, intervalOfErr2 = IntervalOfErr(meanValue, dispersion, p)
     print('Границы интервальной оценки: от ', intervalOfErr2, " до ", intervalOfErr1)
-
-
-    # elif metod == 2:
-    #     ##Error = int(input('Введите предел допускаемого значения основной погрешности i - го блока ИИС'))
-    #     error = 1
-    #     sdOfBasicErr = SDOfBasicErr(error)
-    #     print('Значение СКО основной погрешности', sdOfBasicErr)
-    #
-    #     ##DiffOfInfluenceQuantity = int(input('Введите приращение j-й влияющей величины, для которой нормирована МХ'))
-    #     diffOfInfluenceQuantity = 4
-    #     ##Coef = int(input('Введите значение коэффициента'))
-    #     coef = 0.5
-    #     ##InfluenceQuantity = int(input('Введите влияющую величину'))
-    #     influenceQuantity = 1
-    #     ##ValueOfInfluenceQuantity = int(input('Введите значение влияющей величины'))
-    #     valueOfInfluenceQuantity = 7
-    #     ##ErrOfInfluenceQuantity = int(input('Введите допускаемое изменение погрешности блока ИИС, вызванное отклонением влияющей величины от норм.знач - я'))
-    #     errOfInfluenceQuantity = 1
-    #     valueOfAdditionalErr = ValueOfAdditionalErr(diffOfInfluenceQuantity, coef, influenceQuantity, valueOfInfluenceQuantity, errOfInfluenceQuantity)
-    #     print('Значение наибольшего возможного значения дополнительной погрешности', valueOfAdditionalErr)
-    #
-    #     # вроде не надо,описала лишнего
-    #     # sdOfAdditionalErrInfluenceQuantity = SDOfAdditionalErrInfluenceQuantity(valueOfAdditionalErr)
-    #     # print('Значение СКО дополнительной погрешности от влияющих величин', sdOfAdditionalErrInfluenceQuantity)
-    #
-    #     sdOfAdditionalErrInfluenceQuantity = SDOfAdditionalErrInfluenceQuantity(valueOfAdditionalErr)
-    #     print('Значение СКО дополнительной погрешности i-го блока ИИС от j-й влияющей величины', sdOfAdditionalErrInfluenceQuantity)
-    #     ##NumbOfInfluenceQuantity = int(input('Введите количесво влияющих величин'))
-    #     numbOfInfluenceQuantity = 3
-    #     sdOfRandomErr = SDOfRandomErr(sdOfBasicErr, numbOfInfluenceQuantity,sdOfAdditionalErrInfluenceQuantity)
-    #     print('Значение СКО случайной погрешности блока', sdOfRandomErr)
-    #     ##NumbOfBlocks = int(input('Введите количество блоков, входящих в состав ИК'))
-    #     numbOfBlocks = 2
-    #     sdOfRandomErrOfIK = SDOfRandomErrOfIK(numbOfBlocks, sdOfRandomErr)
-    #     print('Значение СКО случайной погрешности ИК', sdOfRandomErrOfIK)
-    #     ##ConfidProbability = int(input('Введите значение вероятности'))
-    #     confidProbability = 0.95
-    #     intervalOfErr1 = IntervalOfErr2(sdOfRandomErrOfIK, confidProbability)
-    #     intervalOfErr2 = IntervalOfErr2(sdOfRandomErrOfIK, confidProbability)
-    #     print('Значение границ интервала погрешности ИК', intervalOfErr1,intervalOfErr2 )
